9ad37195a9ee4bb6b0aa3e6c0bf08536305752b8
Nghi Nguyen <ninn@gft.com>
2022-03-14 19:57:13 +0700

    refac: using next image

--- components/banner/index.tsx
+++ components/banner/index.tsx
@@ -10,16 +10,13 @@ export const Banner = () => {
             style={{ backgroundImage: 'url("/water-background.png")' }}
         >
             <div className={styles.header}>
-                {/* <h1 className={styles.title}> */}
                 <Typography variant="h1">
                     Hãy để chúng tôi mang nguồn nước sạch trở về
                 </Typography>
-                {/* </h1> */}
                 <Button className={styles.contactButton} type="primary">
                     Liên Hệ Với Chúng Tôi
                 </Button>
             </div>
-            {/* <img className={styles.bannerImage} src="/water-background.png" /> */}
         </div>
     );
 };
--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,11 +1,20 @@
 import React from "react";
+import Image from "next/image";
 import { Nav } from "../nav";
 import styles from "./header.module.scss";
 
 export const Header = () => {
     return (
         <div className={styles.container}>
-            <img src="/logo.png" className={styles.logo} />
+            <div className={styles.logo}>
+                <Image
+                    src="/logo.png"
+                    width={60}
+                    height={50}
+                    layout="responsive"
+                    alt="Thủy Siêu Logo"
+                />
+            </div>
             <Nav />
         </div>
     );
